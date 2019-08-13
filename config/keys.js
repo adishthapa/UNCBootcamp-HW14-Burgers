955943ddfda72b1c50e717c57572ba80b3b632b5
adishthapa <adishthapa@outlook.com>
2019-08-13 18:37:04 -0400

    Initial skeleton files

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+exports.mySQLKeys = {
+    mysql_host: process.env.MYSQL_HOST,
+    mysql_port: process.env.MYSQL_PORT,
+    mysql_user: process.env.MYSQL_USER,
+    mysql_password: process.env.MYSQL_PASSWORD
+};
