acb9be031ee9dfd0706f147245fd64398b33cd76
adishthapa <adishthapa@outlook.com>
2019-08-15 16:39:05 -0400

    Created Router object for burgers

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,32 @@
+var burgers = require("../models/burger.js");
+var express = require("express");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    burgers.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burgers.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    burgers.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        };
+    });
+});
+
+module.exports = router;
