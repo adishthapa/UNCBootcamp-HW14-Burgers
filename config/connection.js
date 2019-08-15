4fafa6a2527ca89820e312d8c74e9dd321a9f776
adishthapa <adishthapa@outlook.com>
2019-08-15 17:41:55 -0400

    Implemented the server for the app

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,11 @@
-require("dotenv").config();
-var keys = require("./keys.js");
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: keys.mySQLKeys.mysql_host,
-    port: keys.mySQLKeys.mysql_port,
-    user: keys.mySQLKeys.mysql_user,
-    password: keys.mySQLKeys.mysql_password,
-    database: "burgers_db"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
